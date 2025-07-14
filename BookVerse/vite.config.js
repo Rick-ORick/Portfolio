d9e78142839abe44da2485e20153847e22156f48
Rick-ORick <chsilvaoliveira@outlook.com>
2025-07-14 14:36:49 -0300

    Update vite.config.js

--- BookVerse/vite.config.js
+++ BookVerse/vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-  },
+  root: '.', // Optional, if your files are in root
+  build: {
+    rollupOptions: {
+      input: '/bookverse.html'
+    }
+  }
 })
